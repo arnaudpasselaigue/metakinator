b2d3bc9de3f22d188810cc3481b82aa5e6c26d09
Aschen <amaret93@gmail.com>
2016-03-31 16:53:31 +0200

    Add Daneel + Question

---
+++ app/src/main/java/com/ia/android/akinasport/models/Question.java
@@ -0,0 +1,11 @@
+package com.ia.android.akinasport.models;
+
+/**
+ * Created by Aschen on 31/03/2016.
+ */
+public class Question
+{
+    private int         id;
+    private String      title;
+
+}
---
+++ app/src/main/java/com/ia/android/akinasport/services/Daneel.java
@@ -0,0 +1,20 @@
+package com.ia.android.akinasport.services;
+
+import com.ia.android.akinasport.models.Question;
+import com.ia.android.akinasport.models.Sport;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Aschen on 31/03/2016.
+ */
+public class Daneel
+{
+    private ArrayList<Question>     m_questions;
+    private ArrayList<Sport>        m_sports;
+
+    public Daneel()
+    {
+
+    }
+}
